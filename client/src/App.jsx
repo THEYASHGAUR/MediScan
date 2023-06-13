9e86f2da9f60cebc7a10da3df2927b1cc298cb9e
Ritu <ritutec001@gmail.com>
2023-06-13 15:19:53 +0530

    ...

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,15 @@
 
+import Footer from "./component/Footer"
+import Navbar from "./component/Navbar"
+
 
 function App() {
  
 
   return (
     <>
-     <h1 className="text-3xl font-bold underline ">
-      Hello world!
-    </h1>
+    <Navbar /> 
+    <Footer />
      
      
     </>
